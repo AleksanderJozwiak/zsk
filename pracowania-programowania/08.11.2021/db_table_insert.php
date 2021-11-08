ba89e2c0384f1a91890fe1c61d302f4bc781b32e
Aleksander Jóźwiak <70820426+AleksanderJozwiak@users.noreply.github.com>
2021-11-08 15:44:26 +0100

    Add files via upload

---
+++ pracowania-programowania/08.11.2021/db_table_insert.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+  <meta charset="utf-8">
+  <title>Użytkownicy</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+  require_once 'scripts/connect.php';
+  $sql = "SELECT * FROM `user`"; //TO NIE APOSTROFY TYLKO TO POD ~
+
+  $rezultat = $connect->query($sql);
+
+  if (isset($_GET['deleteUser'])) {
+    echo "Usunięto użytkownika o id = $_GET[deleteUser]<hr>";
+  }
+
+  echo "<table>";
+  echo <<< TABLE
+    <tr>
+        <th>Id</th>
+        <th>Imię</th>
+        <th>Nazwisko</th>
+        <th>Data urodzenia</th>
+        <th></th>
+    </tr>
+  TABLE;
+  while($rows = $rezultat->fetch_assoc()){
+    echo <<<ROW
+    <tr>
+        <td>
+            $rows[id]
+        </td>
+        <td>
+            $rows[name]
+        </td>
+        <td>
+            $rows[surname]
+        </td>
+        <td>
+            $rows[birthday]
+        </td>
+        <td>
+          <a href="scripts/delete.php?id=$rows[id]">Usuń</a>
+        </td>
+    </tr>
+    ROW;
+  }
+  echo "</table><hr>";
+
+  if (!isset($_GET['addUser'])) {
+    echo '<a href="db_table_insert.php?addUser=">Dodawanie użytkownika</a>';
+  }else {
+    echo <<< ADDUSERFORM
+    <h4>Dodawanie użytkownika</4><br>
+    <form action="./scripts/insert.php" method="post">
+      <input type="text" name="name" placeholder="Podaj imię"><br><br>
+      <input type="text" name="surname" placeholder="Podaj nazwisko"><br><br>
+      <input type="date" name="birthday" placeholder="Data urodzenia"><br><br>
+      <input type="submit" value="Dodaj użytkownika">
+    </form>
+    ADDUSERFORM;
+  }
+
+ ?>
+
+
+</html>
---
+++ pracowania-programowania/08.11.2021/scripts/connect.php
@@ -0,0 +1,3 @@
+<?php
+  $connect = new mysqli("localhost","root","","user");//serwer-user-hasło-baza
+ ?>
