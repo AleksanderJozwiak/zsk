26245ac0d08ce3363689509c38d3f2f37d7b282b
Aleksander Jóźwiak <70820426+AleksanderJozwiak@users.noreply.github.com>
2021-11-09 07:54:54 +0100

    Add files via upload

---
+++ pracowania-programowania/09.11.2021/scripts/insert.php
@@ -0,0 +1,20 @@
+<?php
+  if(!empty($_POST)){
+    foreach ($_POST as $key => $value) {
+      if (empty($value)) {
+        header('location: ../db_table_insert.php?error=Wypełnij wszystkie pola&addUser=');
+        exit();
+      }
+    }
+      require_once "connect.php";
+      $sql = "INSERT INTO `user`(`id`, `cityid`, `name`, `surname`, `birthday`, `height`) VALUES (NULL, '$_POST[cityid]', '$_POST[name]', '$_POST[surname]', '$_POST[birthday]', '$_POST[height]')";
+      $connect->query($sql);
+
+      if($connect->affected_rows){
+        header('location: ../db_table_insert.php?error=Prawidłowo dodano użytkownika');
+      }
+      else {
+        header('location: ../db_table_insert.php?error=Nie udało się dodać użytkownika');
+      }
+  }
+ ?>
